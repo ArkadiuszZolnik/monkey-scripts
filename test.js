7edc8af742423795e7d433509e95d3e33408e1e4
Arkadiusz Zolnik <arkadiusz.zolnik@crowtech.pl>
2024-12-04 08:57:45 +0100

    Test

---
+++ test.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name            Testy git
+// @namespace       http://tampermonkey.net/
+// @version         0.0.1
+// @author          adamaru
+// @match           *
+// @grant           none
+// @license         MIT
+// @run-at			document-start
+// ==/UserScript==
+
+console.log('test');
